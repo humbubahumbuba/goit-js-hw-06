1f33c27f6d02a487a26da89cc95fa0fa69bb0858
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2022-12-04 22:13:36 +0200

    task8-9

--- js/task-08.js
+++ js/task-08.js
@@ -5,3 +5,25 @@
 // где имя поля будет именем свойства, а значение поля - значением свойства.
 // Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const formSubmitBtn = document.querySelector(".login-form");
+
+const onSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля мають бути заповнені");
+  }
+  const objForm = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(objForm);
+
+  event.currentTarget.reset();
+};
+
+formSubmitBtn.addEventListener("submit", onSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+// Напиши скрипт, который изменяет цвета фона элемента < body > через инлайн стиль при клике на button.
+// change - color и выводит значение цвета в span.color.
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.body;
+const buttonEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = bodyEl.style.backgroundColor;
+});
